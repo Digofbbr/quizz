22f443f255c4aa01f90274d7628a05d3b10789e3
Rodrigo Barreto <rodrigofb06@gmail.com>
2022-06-09 18:27:37 -0300

    correção da base_url para pegar as questoes em produção

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+// local: const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = "https://quizz-liard-kappa.vercel.app/api"
+
 
 export default function Home() {
   const router = useRouter()
